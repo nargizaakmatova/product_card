59dffa454575df73881970aaec68fbcc673b45af
Nargiza Kuvanova <nargizaakmatova@gmail.com>
2026-03-26 23:52:55 -0500

    Исправил логику смены цвета карточки

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const recolorFirstCardButton = document.getElementById('recolor-button');
 const firstCatalogCard = document.querySelector('.product-card');
 const blueHashColor = "#0000ff";
 
-recolorFirstCardButton.addEventListener('click', ()=> {
+recolorFirstCardButton.addEventListener('click', ()=>{
   firstCatalogCard.style.backgroundColor = blueHashColor;
-
 });
 
 const greenHashColor = '#00ff44';
@@ -12,21 +11,14 @@ const greenHashColor = '#00ff44';
 const catalogCards = document.querySelectorAll('.product-card');
 const recolorAllCardButton = document.getElementById('recolor-all-button');
 
-recolorAllCardButton.addEventListener('click', ()=> {
+recolorAllCardButton.addEventListener('click', ()=>{
     catalogCards.forEach(
-    card => card.style.backgroundColor = greenHashColor)
-
-  })
-
+    card => card.style.backgroundColor = greenHashColor) })
   const googleOpenButton = document.getElementById('open-google-button');
-
-  googleOpenButton.addEventListener('click', openGoogle);
-
+  googleOpenButton.addEventListener('click', openGoogle)
   function openGoogle() {
     const answer = confirm('Вы действительно хотите перейти на сайт Google?')
-
     if (answer === true) {
       window.open('https://google.com');
     }
   }
-
