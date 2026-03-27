17d06f86a622e0a40c13300e97668add022118db
Nargiza Kuvanova <nargizaakmatova@gmail.com>
2026-03-27 18:17:48 -0500

    style: добавил пробел в стрелочной функции по просьбе ментора

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const recolorFirstCardButton = document.getElementById('recolor-button');
 const firstCatalogCard = document.querySelector('.product-card');
 const blueHashColor = "#0000ff";
 
-recolorFirstCardButton.addEventListener('click', ()=>{
+recolorFirstCardButton.addEventListener('click', () =>{
   firstCatalogCard.style.backgroundColor = blueHashColor;
 });
 
@@ -11,7 +11,7 @@ const greenHashColor = '#00ff44';
 const catalogCards = document.querySelectorAll('.product-card');
 const recolorAllCardButton = document.getElementById('recolor-all-button');
 
-recolorAllCardButton.addEventListener('click', ()=>{
+recolorAllCardButton.addEventListener('click', () =>{
     catalogCards.forEach(
     card => card.style.backgroundColor = greenHashColor) })
   const googleOpenButton = document.getElementById('open-google-button');
