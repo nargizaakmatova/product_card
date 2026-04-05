e2aa88113657d9b41b7f1945e51006f0382ecc7f
Nargiza Kuvanova <nargizaakmatova@gmail.com>
2026-04-05 00:27:44 -0500

    style: fix spacing and indentation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const recolorFirstCardButton = document.getElementById('recolor-button');
 const firstCatalogCard = document.querySelector('.product-card');
 const blueHashColor = "#0000ff";
 
-recolorFirstCardButton.addEventListener('click', () =>{
+recolorFirstCardButton.addEventListener('click', () => {
   firstCatalogCard.style.backgroundColor = blueHashColor;
 });
 
@@ -11,13 +11,17 @@ const greenHashColor = '#00ff44';
 const catalogCards = document.querySelectorAll('.product-card');
 const recolorAllCardButton = document.getElementById('recolor-all-button');
 
-recolorAllCardButton.addEventListener('click', () =>{
-    catalogCards.forEach(
-    card => card.style.backgroundColor = greenHashColor) })
-  const googleOpenButton = document.getElementById('open-google-button');
-  googleOpenButton.addEventListener('click', openGoogle)
-  function openGoogle() {
-    const answer = confirm('Вы действительно хотите перейти на сайт Google?')
+recolorAllCardButton.addEventListener('click', () => {
+    catalogCards.forEach(card => {
+       card.style.backgroundColor = greenHashColor;
+    });
+});
+
+const googleOpenButton = document.getElementById('open-google-button');
+googleOpenButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+    const answer = confirm('Вы действительно хотите перейти на сайт Google?');
     if (answer === true) {
       window.open('https://google.com');
     }
